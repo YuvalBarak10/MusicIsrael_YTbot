62156382cedc7cf6d00502d679f402a6035c3489
YuvalBarak10 <31255153+YuvalBarak10@users.noreply.github.com>
2020-05-04 02:00:58 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import telebot
+import converter
+import time
+
+url = ''
+mp3_file = ''
+bot = telebot.TeleBot('854466744:AAHXYY0hQmQiBNOto3dNK8734uiZUhwETro')
+
+@bot.message_handler(commands=['start'])
+def start_message(message):	
+    bot.send_message(message.chat.id, 'רובוט ראשונים במוזיקה \n מוריד ועורך מיוטיוב')
+
+#@bot.message_handler(content_types=['text'])
+#def echo_message(message):
+    #bot.reply_to(message, message.text)
+
+@bot.message_handler(func=lambda message: True)
+def get_url(message):
+	bot.send_message(message.chat.id, 'טוען...')
+	try:
+		url = message.text
+		mp3_file = converter.convert(url)
+		#bot.send_message(message.chat.id, 'השיר\n{} \nיורד ונערך...'.format(mp3_file[6 : -4]))
+		bot.reply_to(message, 'השיר\n{} \nיורד ונערך...'.format(mp3_file[4 : -4]))
+		audio = open(mp3_file, 'rb')
+		bot.send_audio(message.chat.id,audio,'🎵@MusicIsrael🎧','','ראשונים במוזיקה','{}'.format(mp3_file[4:-4]))
+		converter.delete(mp3_file)
+	except:
+		#converter.delete(mp3_file)
+		bot.send_message(message.chat.id, 'ראשונים במוזיקה!')
+
+bot.polling()
---
+++ converter.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import youtube_dl
+import os
+
+def convert(url):
+	dirName = 'mp3'
+	if not os.path.exists(dirName):
+		os.mkdir(dirName)
+	options = {
+		'outtmpl':('./mp3/%(title)s.%(ext)s'),
+	    'format': 'bestaudio/best',
+	    'postprocessors': [{
+	        'key': 'FFmpegExtractAudio',
+	        'preferredcodec': 'mp3',
+	        'preferredquality': '320',
+	    }],
+	}      
+	try:
+		title = os.listdir("./mp3")
+		with youtube_dl.YoutubeDL(options) as ydl:
+			info = ydl.extract_info(url, download=True)
+			nm = ydl.prepare_filename(info)   # get /mp3/filename.webm
+			mp3_file = nm[:-4] + 'mp3'		  # get /mp3/filename.mp3
+			return mp3_file
+	except:
+		return 'UrlError'
+
+def delete(audio):
+	os.remove(audio)
