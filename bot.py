e6c9395c9ab40fd6a005822d4a3f792413f651db
YuvalBarak10 <31255153+YuvalBarak10@users.noreply.github.com>
2020-05-04 19:07:31 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,9 +17,8 @@ def start_message(message):
 
 @bot.message_handler(func=lambda message: True)
 def get_url(message):
-	#dele = bot.reply_to(message,'טוען...')
+	dele = bot.reply_to(message,'טוען...')
 	try:
-		dele = bot.reply_to(message,'טוען...')
 		url = message.text
 		mp3_file = converter.convert(url)
 		#bot.edit_message_text('ממיר ל - mp3...',message.chat.id,dele.message_id)
